1537c25d255bf96b42ace1a06812781dc0dac9d4
Alex Eulenberg <aeulenbe@adobe.com>
2023-01-27 10:50:41 -0800

    better error catching

--- app.py
+++ app.py
@@ -12,19 +12,22 @@ def hello():
     out = [
         '<form>Number to find square root of: <input type=text name=num> <input type=submit></form>',
     ]
+    nb = None
     if ns:
         try:
-            nb = int(ns)
-        except:
-            return "<p>bad num {ns}</p>"
-    else:
-         out.append(f"<p>no num! using {default_num}</p>")
+            nb = float(ns)
+        except ValueError:
+            out.append(f"<p>bad num {ns}</p>")
+    if nb is None:
+         out.append(f"<p>using {default_num}</p>")
          nb = default_num
 
     match [ nb ]:
         case [ number ]:
-            out.append(f"<p>The square root of {nb} is: {math.sqrt(number)}</p>")
-
+            if number < 0:
+                out.append(f"<p>Can't find square root of {nb}</p>")
+            else:
+                out.append(f"<p>The square root of {nb} is: {math.sqrt(number)}</p>")
     return '\n'.join(out) + '\n'
     
 
