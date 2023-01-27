c47f5efce7f871fb6aa46123f9b512a61439727b
Alex Eulenberg <aeulenbe@adobe.com>
2023-01-26 16:02:43 -0800

    back to normal

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask import request
 import os
 import math
 
@@ -7,23 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    ns = request.args.get('num')
-    if ns:
-        try:
-            nb = int(ns)
-        except:
-            return "bad num {ns}"
-    else:
-         return "no num!"
-        
     out = [] 
     match [ nb ]:
         case [ number ]:
             out.append(f"Look at this square root: {math.sqrt(number)}\n")
     
-    for key, value in os.environ.items():
-        out.append(f"{key}={value}\n")
-            
     return ''.join(out)
     
 
