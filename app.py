4cea7674ad8b8d6ea22f0b2f9f76eba622b5fa52
Alex Eulenberg <aeulenbe@adobe.com>
2023-01-27 10:18:04 -0800

    even better

--- app.py
+++ app.py
@@ -11,21 +11,19 @@ def hello():
     ns = request.args.get('num')
     out = [
         '<form>Number to find square root of: <input type=text name=num> <input type=submit></form>',
-        '<pre>',
     ]
     if ns:
         try:
             nb = int(ns)
         except:
-            return "bad num {ns}"
+            return "<p>bad num {ns}</p>"
     else:
-         out.append(f"no num! using {default_num}")
+         out.append(f"<p>no num! using {default_num}</p>")
          nb = default_num
 
     match [ nb ]:
         case [ number ]:
-            out.append(f"The square root of {nb} is: {math.sqrt(number)}\n")
-    out.append('</pre>')
+            out.append(f"<p>The square root of {nb} is: {math.sqrt(number)}</p>")
 
     return '\n'.join(out) + '\n'
     
